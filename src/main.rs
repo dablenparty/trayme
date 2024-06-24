be17fc06f67f0eb8e8faa555d2b7ceb71621e648
dablenparty <hdavenport8902@gmail.com>
2024-06-24 12:32:48 -0400

    Add functions to build tray menu

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
+use anyhow::Context;
+use strum::VariantArray;
 use tao::event_loop::EventLoopBuilder;
+use tray_icon::{
+    menu::{Menu, MenuItemBuilder},
+    TrayIcon, TrayIconBuilder,
+};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, strum::VariantArray)]
 enum TrayMessage {
     Kill,
     ShowLogs,
+}
+
+impl std::fmt::Display for TrayMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TrayMessage::Kill => write!(f, "Kill"),
+            TrayMessage::ShowLogs => write!(f, "Show Logs"),
+        }
+    }
+}
+
+fn build_tray_menu() -> anyhow::Result<Menu> {
+    let menu = Menu::new();
+    for msg in TrayMessage::VARIANTS {
+        let item = MenuItemBuilder::new()
+            .text(msg.to_string())
+            .id(msg.into())
+            .build();
+        menu.append(&item)?;
+    }
+    Ok(menu)
+}
+
+fn build_tray() -> anyhow::Result<TrayIcon> {
+    let menu = build_tray_menu()?;
+    TrayIconBuilder::new()
+        .with_menu(Box::new(menu))
+        .with_tooltip("PROCESS_NAME")
+        .build()
+        .context("Failed to build tray icon")
 }
 
 fn run_event_loop() -> anyhow::Result<()> {
@@ -16,7 +52,7 @@ fn run_event_loop() -> anyhow::Result<()> {
 
     // TODO: log all output from the wrapped command
 
-    event_loop.run(|_event, _window, control_flow| {
+    event_loop.run(move |_event, _window, control_flow| {
         *control_flow = tao::event_loop::ControlFlow::Exit;
     })
 }
